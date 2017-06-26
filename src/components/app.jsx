f886c9557a7809ad1413f4480d82478ecff16380
tanvir002700 <tanvir002700@gmail.com>
2017-06-27 01:49:55 +0600

    add user to app component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {firebaseApp} from '../config/firebase';
 
 class App extends Component {
@@ -6,10 +7,19 @@ class App extends Component {
         firebaseApp.auth().signOut();
     }
 
+    renderUserDetails() {
+        console.log(this.props);
+        const {user} = this.props;
+        return(
+            <div>email: {user.email}</div>
+        );
+    }
+
     render() {
         return(
             <div>
                 <h2>Home page</h2>
+                {this.renderUserDetails()}
                 <button type="button" className="btn btn-danger"
                     onClick={() => this.signOut()}
                 >Sign Out</button>
@@ -18,4 +28,10 @@ class App extends Component {
     };
 }
 
-export default App;
+function mapStateToProps(state) {
+    console.log(state);
+    const {user} = state;
+    return {user};
+}
+
+export default connect(mapStateToProps, null)(App);
